c811fdaecbf6ffa17b2de7fc9e572a834f01a060
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-24 20:20:59 -0300

    código finalizado

--- ExArray/Program.cs
+++ ExArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExArray
 {
@@ -6,7 +7,22 @@ namespace ExArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Entre com um número de pessoas: "); //Criando a variável de entrada
+            int n = int.Parse(Console.ReadLine()); //Criando vetor com o número da variável de entrada
+            double[] vetor = new double[n]; 
+            for (int i = 0; i < n; i++) //Utilização do comando for para percorrer o vetor
+            {
+                Console.Write("Altura da pessoa: ");
+                vetor [i] = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture); //Entrada dos elementos no vetor pelo usuário
+            }
+            double soma = 0; //Variável soma      
+            for (int i = 0; i < n; i++) //Utilização do for para percorrer o vetor
+            {
+                soma += vetor[i]; //Soma dos elementos inseridos
+            }
+            double media = soma / n; //Declaração da variável media
+            Console.WriteLine("A média total das pessoas inseridas é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
         }
     }
 }
